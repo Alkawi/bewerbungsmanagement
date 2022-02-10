ec7d4b31524bd8cb7dc796403ae49595d6302f2e
Alkawi <alexander.kaiser1996@web.de>
2022-02-10 13:17:00 +0100

    Add css classes to AddForm inputs

--- src/app/components/AddForm/AddForm.tsx
+++ src/app/components/AddForm/AddForm.tsx
@@ -7,37 +7,59 @@ function AddForm(): JSX.Element {
       <label>
         Unternehmen
         <br />
-        <input type="text" placeholder="Muster GmbH" required />
+        <input
+          type="text"
+          placeholder="Muster GmbH"
+          required
+          className={styles['form-input']}
+        />
       </label>
       <label>
         Stellenbezeichnung
         <br />
-        <input type="text" placeholder="Web Developer" required />
+        <input
+          type="text"
+          placeholder="Web Developer"
+          required
+          className={styles['form-input']}
+        />
       </label>
       <label>
         Datum der Bewerbung
         <br />
-        <input type="date" required />
+        <input type="date" required className={styles['form-input']} />
       </label>
       <label>
         Ansprechpartner*in
         <br />
-        <input type="text" placeholder="Frau Müller" />
+        <input
+          type="text"
+          placeholder="Frau Müller"
+          className={styles['form-input']}
+        />
       </label>
       <label>
         Kontakt Telefonnummer
         <br />
-        <input type="tel" placeholder="0123456789" />
+        <input
+          type="tel"
+          placeholder="0123456789"
+          className={styles['form-input']}
+        />
       </label>
       <label>
         Kontakt E-Mail
         <br />
-        <input type="email" placeholder="mueller@muster-gmbh.de" />
+        <input
+          type="email"
+          placeholder="mueller@muster-gmbh.de"
+          className={styles['form-input']}
+        />
       </label>
       <label>
         Aktueller Status
         <br />
-        <select>
+        <select className={styles['form-input']}>
           <option value="ausstehend">Bewerbung ausstehend</option>
           <option value="beworben">Beworben</option>
           <option value="interview">Vorstellungsgespräch</option>
@@ -49,12 +71,12 @@ function AddForm(): JSX.Element {
       <label>
         Letzter Kontakt
         <br />
-        <input type="date" />
+        <input type="date" className={styles['form-input']} />
       </label>
       <label>
         Rückmeldung bis
         <br />
-        <input type="date" />
+        <input type="date" className={styles['form-input']} />
       </label>
       <Button>Speichern</Button>
     </form>
